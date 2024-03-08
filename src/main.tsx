0e97d59eab574102ab3b4e1599b5706f1651434a
heechul94 <yhcu3517@gmail.com>
2024-03-08 09:48:09 +0900

    [MOD]: apply query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { Provider } from "react-redux";
+import store from "./shared/redux/config/configStore.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
