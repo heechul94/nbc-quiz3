69aef20aab3bd77d6f949e123bcd9d156269c231
heechul94 <yhcu3517@gmail.com>
2024-03-08 09:51:19 +0900

    [MOD]: apply todo component

--- src/components/todoList/TodoList.tsx
+++ src/components/todoList/TodoList.tsx
@@ -1,9 +1,28 @@
-import AddForm from "../addForm/AddForm";
+import { useTodoQuery } from "@/hooks/useTodoQuery";
+import Todo from "../todo/Todo";
+import { TTodo } from "@/types/types";
 
-const TodoList = () => {
+const useTodoList = () => {
+  const { data: todos, isLoading } = useTodoQuery();
+
+  return { todos, isLoading };
+};
+
+const TodoList = ({ isDone }: { isDone: boolean }) => {
+  const { todos, isLoading } = useTodoList();
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <div>
-      <AddForm />
+      <p>{isDone ? "Done!!" : "Working..."}</p>
+      <ul>
+        {todos
+          ?.filter((todo: TTodo) => isDone === todo.isDone)
+          .map((todo: TTodo) => (
+            <Todo key={todo.id} todo={todo} />
+          ))}
+      </ul>
     </div>
   );
 };
